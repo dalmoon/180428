6bfcafef0deedc11fb57af6aa6bdd778f82f91dc
ss <ss@ss-PC105>
2018-04-28 12:49:01 +0900

    18.04.28

--- java_day01/src/java_day01/HapTest.java
+++ java_day01/src/java_day01/HapTest.java
@@ -1,5 +1,10 @@
 package java_day01;
 
 public class HapTest {
-	
+	public static void main(String[] args) {
+		int a = 1; // 개발자가 변수선언 및 초기화(기본값(연속값))
+		a = 2; // 개발자가 실값을 대입 변경
+		System.out.println(a + 2);
+		System.out.println(a - 2);
+	}
 }
